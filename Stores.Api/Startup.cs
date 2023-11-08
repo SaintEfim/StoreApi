8b8381248ce32f113a4cc5f8e278c560506937c5
efedenko <saintefim2@gmail.com>
2023-11-08 10:09:27 +0300

    rename My Api

--- Stores.Api/Startup.cs
+++ Stores.Api/Startup.cs
@@ -52,7 +52,7 @@ namespace Stores.Api
             // Добавьте конфигурацию Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Stores API", Version = "v1" });
             });
         }
 
