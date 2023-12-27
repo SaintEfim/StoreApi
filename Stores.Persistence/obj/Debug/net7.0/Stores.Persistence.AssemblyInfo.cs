a2afbcf1a5418d89d6b27ce6e0224ae3e4d465ec
efedenko <saintefim2@gmail.com>
2023-12-27 16:46:32 +0300

    init

--- Stores.Persistence/obj/Debug/net7.0/Stores.Persistence.AssemblyInfo.cs
+++ Stores.Persistence/obj/Debug/net7.0/Stores.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stores.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a99dc86ab2d4e8ba45ae5b1e0616d8353c8d6a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e611aa2c006d0d83d931cdc08dea94a18eb609e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stores.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stores.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
